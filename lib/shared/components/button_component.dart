0e61c5bd26dd5eb4535d51d7c0be9973a2edb12c
Brandao Developer <dannnbrandao@gmail.com>
2021-05-13 01:28:08 -0300

    Criando um botão customizado

---
+++ lib/shared/components/button_component.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ButtonComponent extends StatelessWidget {
+  final String label;
+  final Function onPressed;
+
+  const ButtonComponent({
+    Key key,
+    this.label,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: OutlinedButton.icon(
+        label: Text(label),
+        icon: Icon(Icons.add_to_home_screen_rounded),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
